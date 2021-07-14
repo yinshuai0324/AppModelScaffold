db63d22691ce54a6b80a85d6354fa9ec94264b86
yinshuai <yinshuai@angogo.cn>
2021-07-14 15:00:52 +0800

    Splash页面优化

--- business/model-splash/src/main/java/com/model/splash/SplashMainActivity.kt
+++ business/model-splash/src/main/java/com/model/splash/SplashMainActivity.kt
@@ -1,17 +1,36 @@
 package com.model.splash
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
+import android.animation.Animator
 import com.alibaba.android.arouter.launcher.ARouter
+import com.library.base.activity.BaseActivity
+import com.library.base.viewmodel.BaseViewModel
+import com.model.splash.databinding.SplashActivityMainBinding
 
-class SplashMainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_activity_main)
+class SplashMainActivity : BaseActivity<BaseViewModel, SplashActivityMainBinding>(),
+    Animator.AnimatorListener {
 
-        findViewById<Button>(R.id.jumpBtn).setOnClickListener {
-            ARouter.getInstance().build("/model/HomeMainActivity").navigation()
-        }
+    override fun initData() {
+        viewBinding.animView.addAnimatorListener(this)
+        viewBinding.animView.playAnimation()
+    }
+
+    override fun createdObserve() {
+
+    }
+
+    override fun onAnimationStart(p0: Animator?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animator?) {
+        ARouter.getInstance().build("/model/HomeMainActivity").navigation()
+    }
+
+    override fun onAnimationCancel(p0: Animator?) {
+
+    }
+
+    override fun onAnimationRepeat(p0: Animator?) {
+
     }
 }
